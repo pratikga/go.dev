ddfa9c3adc1ee17defb631a76c9f1da0eb36bdb6
pratikga <pratik_ga@yahoo.com>
2022-10-01 00:15:38 -0500

    updating basics folder

---
+++ Basics/Basics/short-variable-declarations.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var i, j int = 10, 20
+
+func main() {
+	s := "string"
+	k := 10
+	fmt.Println(i, j, k, s)
+}
---
+++ Basics/Basics/variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var a, b, c, d bool
+
+func main() {
+	var e int
+	fmt.Println(a, b, c, d, e)
+
+}
